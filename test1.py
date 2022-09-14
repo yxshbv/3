d130da9a80231e1df4b9617d33db3c1e6e41904f
yxshbv <103039015+yxshbv@users.noreply.github.com>
2022-09-14 10:28:22 +0500

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,29 @@
+i=int(input())
+
+if i == 1:
+
+    print("Monday")
+
+elif i == 2:
+
+    print("Tuesday")
+
+elif i == 3:
+
+    print("Wednesday")      
+
+elif i == 4:
+
+    print("Thursday")
+
+elif i == 5:
+
+    print("Friday")
+
+elif i == 6:
+
+    print("Saturday")
+
+elif i == 7:
+
+    print("Sunday")
